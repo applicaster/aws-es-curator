b10c50538d7c002ff722cfc302b1df77aefbc2d1
zloof <nirzloof@gmail.com>
2020-07-08 17:58:54 +0300

    using boto3

--- delete-indexes.py
+++ delete-indexes.py
@@ -1,18 +1,24 @@
 import boto3
+import os
 from requests_aws4auth import AWS4Auth
 from elasticsearch import Elasticsearch, RequestsHttpConnection
 import curator
 
-host = 'search-audience-us1-t47naux7n5bvu3wkapaiz3nzdu.us-east-1.es.amazonaws.com' # For example, search-my-domain.region.es.amazonaws.com
-region = 'us-east-1' # For example, us-west-1
+source_type    = os.environ.get("source_type","name")
+direction_type = os.environ.get("direction_type","older")
+delete_after   = int(os.environ.get("delete_after",30))
+idx_format     = os.environ.get("index_format","%Y.%m.%d")
+unit_type      = os.environ.get("unit_type","days")
+host           = os.environ.get("es_endpoint") # For example, search-my-domain.region.es.amazonaws.com
+if not host:
+    raise Exception("[es_endpoint] OS variable is not set")
+
+region = host.split(".")[1] # For example, us-west-1
 service = 'es'
 credentials = boto3.Session().get_credentials()
 awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
 
-# Lambda execution starts here.
-# def lambda_handler(event, context):
-
-    # Build the Elasticsearch client.
+# Build the Elasticsearch client.
 es = Elasticsearch(
     hosts = [{'host': host, 'port': 443}],
     http_auth = awsauth,
@@ -21,27 +27,10 @@ es = Elasticsearch(
     connection_class = RequestsHttpConnection
 )
 
-# A test document.
-document = {
-    "title": "Moneyball",
-    "director": "Bennett Miller",
-    "year": "2011"
-}
-
-# Index the test document so that we have an index that matches the timestring pattern.
-# You can delete this line and the test document if you already created some test indices.
-es.index(index="movies-2017.01.31", doc_type="movie", id="1", body=document)
-
 index_list = curator.IndexList(es)
 
 # Filters by age, anything with a time stamp older than 30 days in the index name.
-index_list.filter_by_age(source='name', direction='older', timestring='%Y.%m.%d', unit='days', unit_count=30)
-
-# Filters by naming prefix.
-# index_list.filter_by_regex(kind='prefix', value='my-logs-2017')
-
-# Filters by age, anything created more than one month ago.
-# index_list.filter_by_age(source='creation_date', direction='older', unit='months', unit_count=1)
+index_list.filter_by_age(source=source_type, direction=direction_type, timestring=idx_format, unit=unit_type, unit_count=delete_after)
 
 print("Found %s indices to delete" % len(index_list.indices))
 
